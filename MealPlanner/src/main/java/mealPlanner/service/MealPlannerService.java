8db416115b48e539dc16929a57f90a511d86740b
leakkari <lea.akkary@gmail.com>
2020-02-15 17:17:27 -0500

    EnterOwnIngredients implmented

--- MealPlanner/src/main/java/mealPlanner/service/MealPlannerService.java
+++ MealPlanner/src/main/java/mealPlanner/service/MealPlannerService.java
@@ -1,9 +1,13 @@
 package mealPlanner.service;
 
 import java.sql.Date;
+import java.util.List;
 
+import mealPlanner.service.InvalidInputException;
 import mealPlanner.model.Day;
+import mealPlanner.model.Ingredient;
 import mealPlanner.model.MealPlannerApp;
+import mealPlanner.model.OwnedIngredient;
 import mealPlanner.model.User;
 import mealPlanner.persistence.PersistenceXStream;
 
@@ -24,7 +28,16 @@ public class MealPlannerService {
 		PersistenceXStream.saveToXMLwithXStream(mp);
 	}
 
-	public User createUser(String username, String password, int calorieGoal) {
+	public User createUser(String username, String password, int calorieGoal) throws InvalidInputException  {
+		
+		// Check if username is already in database
+		List<User> userList = mp.getUsers();
+		for (User u : userList) {
+			if(u.getUsername().contentEquals(username)) {
+				throw new InvalidInputException("This username is taken.");
+			}
+		}
+		
 		Day today = new Day(new Date(0, 0, 0), 0);
 		User u = new User(username, password, calorieGoal, today);
 		mp.addUser(u);
@@ -33,6 +46,47 @@ public class MealPlannerService {
 	}
 	
 	
+	/**
+	 * keep track of ingredients user already has
+	 * @param user
+	 * @param ingredientName
+	 * @param amount
+	 * @return list of own ingredients
+	 * @throws InvalidInputException
+	 */
+	public OwnedIngredient enterOwnIngredient(String username, String ingredientName, int amount) throws InvalidInputException {
+		
+		int index=0;
+		
+		// check ingredientName
+		if(ingredientName.contentEquals(null) || ingredientName.contentEquals("")) {
+			throw new InvalidInputException("Please Enter Valid Ingredient Name");
+		}
+		
+		
+		List<User> userList = mp.getUsers();
+		for(int i=0; i<userList.size(); i++) {
+			if( userList.get(i).getUsername().contentEquals(username)){
+				index=i;
+				break;
+			}		
+		}
+		
+		User user = userList.get(index);
+
+		// Create ingredient if it doesnt exist and add it to the list
+		Ingredient i = new Ingredient(ingredientName);
+		OwnedIngredient owned_i = new OwnedIngredient(amount, i, user);
+		
+		// Add Ingredient
+		user.addOwnedIngredient(owned_i);
+		
+		//PersistenceXStream.saveToXMLwithXStream(mp);
+		return owned_i;
+		
+			
+		
+	}
 	
 	
 }
