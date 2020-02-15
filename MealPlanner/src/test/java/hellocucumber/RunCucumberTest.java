729979d2f751ba970b508b6c72fcf22270c940fb
leakkari <lea.akkary@gmail.com>
2020-02-15 17:53:49 -0500

    Cucumber file fixed

--- MealPlanner/src/test/java/hellocucumber/RunCucumberTest.java
+++ MealPlanner/src/test/java/hellocucumber/RunCucumberTest.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = "pretty", 
+features = "src/test/resources",
+glue = "src/test/java/hellocucumber")
 public class RunCucumberTest {
 }
