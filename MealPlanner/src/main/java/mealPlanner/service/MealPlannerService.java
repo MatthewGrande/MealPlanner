a364f0c6ad2e7f362abcead95dffd3d473bd5195
abed <abed.atassi@mail.mcgill.ca>
2020-02-16 15:45:29 -0500

    Added login validation func and rest mapping

--- MealPlanner/src/main/java/mealPlanner/service/MealPlannerService.java
+++ MealPlanner/src/main/java/mealPlanner/service/MealPlannerService.java
@@ -17,93 +17,103 @@ import mealPlanner.persistence.PersistenceXStream;
 @Service
 public class MealPlannerService {
 	private MealPlannerApp mp;
+
 	public MealPlannerService(MealPlannerApp mp) {
 		this.mp = mp;
 	}
 
-	public User createUser(String username, String password, int calorieGoal) throws InvalidInputException  {
-		
+	public User createUser(String username, String password, int calorieGoal) throws InvalidInputException {
+
 		// Check if username is already in database
 		List<User> userList = mp.getUsers();
 		for (User u : userList) {
-			if(u.getUsername().contentEquals(username)) {
+			if (u.getUsername().contentEquals(username)) {
 				throw new InvalidInputException("This username is taken.");
 			}
 		}
-		
+
 		Day today = new Day(new Date(0, 0, 0), 0);
 		User u = new User(username, password, calorieGoal, today);
 		mp.addUser(u);
 		PersistenceXStream.saveToXMLwithXStream(mp);
 		return u;
 	}
-	
+
 	public User deleteUser(String username, String password) throws InvalidInputException {
 		User u = getUser(username);
-		if(u == null) {
-			throw new InvalidInputException("There is no user with that username");
+
+		if (isValidLogin(username, password)) {
+			mp.removeUser(u);
 		}
-		if(!u.getPassword().equals(password)) {
-			throw new InvalidInputException("Incorrect password");
-		}
-		mp.removeUser(u);
+
 		PersistenceXStream.saveToXMLwithXStream(mp);
 		return u;
 	}
-	
+
 	public User getUser(String username) {
 		List<User> users = mp.getUsers();
-		
-		for(User u : users) {
-			if(u.getUsername().equals(username)){
+
+		for (User u : users) {
+			if (u.getUsername().equals(username)) {
 				return u;
 			}
 		}
 		return null;
 	}
-	
-	
+
+	public Boolean isValidLogin(String userName, String password) throws InvalidInputException {
+
+		User u = getUser(userName);
+		if (u != null) {
+			if (u.getPassword().equals(password)) {
+				return true;
+			} else {
+				throw new InvalidInputException("Incorrect password.");
+			}
+		} else {
+			throw new InvalidInputException("Cannot find account for this username.");
+		}
+	}
+
 	/**
 	 * keep track of ingredients user already has
+	 * 
 	 * @param user
 	 * @param ingredientName
 	 * @param amount
 	 * @return list of own ingredients
 	 * @throws InvalidInputException
 	 */
-	public OwnedIngredient enterOwnIngredient(String username, String ingredientName, int amount) throws InvalidInputException {
-		
-		int index=0;
-		
+	public OwnedIngredient enterOwnIngredient(String username, String ingredientName, int amount)
+			throws InvalidInputException {
+
+		int index = 0;
+
 		// check ingredientName
-		if(ingredientName.contentEquals(null) || ingredientName.contentEquals("")) {
+		if (ingredientName.contentEquals(null) || ingredientName.contentEquals("")) {
 			throw new InvalidInputException("Please Enter Valid Ingredient Name");
 		}
-		
-		
+
 		List<User> userList = mp.getUsers();
-		for(int i=0; i<userList.size(); i++) {
-			if( userList.get(i).getUsername().contentEquals(username)){
-				index=i;
+		for (int i = 0; i < userList.size(); i++) {
+			if (userList.get(i).getUsername().contentEquals(username)) {
+				index = i;
 				break;
-			}		
+			}
 		}
-		
+
 		User user = userList.get(index);
 
 		// Create ingredient if it doesnt exist and add it to the list
 		Ingredient i = new Ingredient(ingredientName);
 		OwnedIngredient owned_i = new OwnedIngredient(amount, i, user);
-		
+
 		// Add Ingredient
 		user.addOwnedIngredient(owned_i);
-		
-		//PersistenceXStream.saveToXMLwithXStream(mp);
+
+		// PersistenceXStream.saveToXMLwithXStream(mp);
 		return owned_i;
-		
-			
-		
+
 	}
-	
-	
+
 }
